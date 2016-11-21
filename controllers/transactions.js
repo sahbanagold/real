d3b05f333ba9c2bc39e7900e5db1adfaa608a48e
sahbana <sahbanalo@gmail.com>
2016-11-22 00:53:38 +0700

    transactions api fixed

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -7,42 +7,44 @@ exports.transactionsGet = function(req,res,next){
     })
 }
 exports.transactionsPost = function(req,res,next){
+  let newItems = []
+  console.log(req.body)
   if(req.body.itemname){
-    let newItems = []
     for (let i = 0; i < req.body.itemname.length; i++) {
       let newitemname = ""
       let newitemprice = ""
       let newitemquantity = ""
       let newitemtotalprice = ""
       if(req.body.itemname[i] !== "") newitemname =  req.body.itemname[i]
-      if(req.body.itemprice[i] !== "") newitemname =  parseInt(req.body.itemprice[i])
-      if(req.body.quantity[i] !== "") newitemname =  parseInt(req.body.quantity[i])
-      if(req.body.totalprice[i] !== "") newitemname =  parseInt(req.body.totalprice[i])
+      if(req.body.itemprice[i] !== "") newitemprice =  parseInt(req.body.itemprice[i])
+      if(req.body.quantity[i] !== "") newitemquantity =  parseInt(req.body.quantity[i])
+      if(req.body.totalprice[i] !== "") newitemtotalprice =  parseInt(req.body.totalprice[i])
       if(newitemname !== "" && newitemprice !== "" && newitemquantity !== "" && newitemtotalprice !== ""){
         newItems.push({
-                name: String,
-                price: Number,
-                quantity: Number,
-                sums: Number
+                name: newitemname,
+                price: newitemprice,
+                quantity: newitemquantity,
+                sums: newitemtotalprice
               })
+              console.log(newItems,"item pushed")
       }
     }
   }
+  console.log(newItems,"tst item")
   if(newItems.length > 0){
-    let newTransactions = new Transactions({
-      userId: req.body.userId,
-      nominal: req.body.nominal,
-      recipient: req.body.penerima,
-      account: req.body.rekening,
-      bank: req.body.bank, 
-      status: "UnPaid",
-      dateRequested: new Date(),
-      notes: req.body.keterangan,
-      items: newItems
-    })
+    let newTransactions = new Transactions()
+      newTransactions.userId= req.body.userId
+      newTransactions.nominal= req.body.nominal
+      newTransactions.recipient= req.body.penerima
+      newTransactions.account= req.body.rekening
+      newTransactions.bank= req.body.bank
+      newTransactions.status= "UnPaid"
+      newTransactions.dateRequested= new Date()
+      newTransactions.notes= req.body.keterangan
+      newTransactions.items= newItems
     newTransactions.save(function (err) {
       if (err){
-        throw err
+        console.log(err)
         return
       }
       res.json({success: true,message: "success save transactions"})
